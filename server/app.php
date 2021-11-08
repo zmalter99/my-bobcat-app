fd30d69edb634356f5278162cf8a852e68c64aac
Zach Malter <zach@maltertech.com>
2021-11-08 09:35:48 -0500

    font change

--- server/app.php
+++ server/app.php
@@ -16,7 +16,7 @@
     <!--CSS & Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link href="/css/app.css?<?php echo time(); ?>" rel="stylesheet">
 
     <!--JS-->
